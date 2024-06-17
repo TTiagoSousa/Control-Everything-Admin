f715cc1fb7c2d7c1e104a54bd6a501fc4636046e
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-17 17:32:18 +0100

    Index Page

--- src/Pages/Public/Index/Index.jsx
+++ src/Pages/Public/Index/Index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Index.scss';
+import Global_Button from '../../../Components/Buttons/Global_Button/Global_Button';
 
 const Index = () => {
 
   return (
     <div className='Index'>
-      
+      <div>
+        <Global_Button
+          Text={("Sign In")}
+          to="Sign_In"
+        />
+      </div>
     </div>
   )
 };
