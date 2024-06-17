4699e44d7eeb8fd102913a8887351f065d4795e1
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-17 17:48:47 +0100

    Header Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import * as Public_Page from './Imports/public.pages';
+import Header_Index from './Containers/Headers/Header_Index/Header_Index';
 
 function App() {
+
+  const location = useLocation();
+  const showHeader = location.pathname === '/' || location.pathname === '/SignIn' || location.pathname === '/SignUp';
 
   return (
     <>
       <main>
 
+        {showHeader && <Header_Index />}
+        
         <Routes>
           <Route index element={<Public_Page.Index />} />
         </Routes>
---
+++ src/Containers/Headers/Header_Index/Header_Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header_Index.scss';
+
+const Header_Index = () => {
+
+  return (
+    <header className='Header_Index'>
+f
+    </header>
+  )
+};
+
+export default Header_Index;
