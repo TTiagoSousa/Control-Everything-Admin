9b8133bf1dd083c9c2e455839803513249d4abbc
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-21 09:54:39 +0100

    Validations, sign up user card

--- src/Pages/Private/Programming/Validations/Validations.jsx
+++ src/Pages/Private/Programming/Validations/Validations.jsx
@@ -10,6 +10,10 @@ const Validations = () => {
           <span>Sign in employee</span>
           <Link target="_blank" to="https://docs.google.com/document/d/1qUhXftXPdI-7o6BzpHeVF3K3EQ7PsTZF_Ex5j6aWUrw/edit?usp=drive_link"><span>Link</span></Link>
         </div>
+        <div className='Card'>
+          <span>Sign up user</span>
+          <Link target="_blank" to="https://docs.google.com/document/d/1l_rEO56soLYtemNOBGPt005rMih5l3-uRHFkloNZqpo/edit?usp=drive_link"><span>Link</span></Link>
+        </div>
       </div>
     </div>
   )
