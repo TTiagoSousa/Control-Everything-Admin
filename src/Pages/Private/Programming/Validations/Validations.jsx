80ccf162d8bc0433bd9a921ca92691bb3833cc35
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-21 15:33:46 +0100

    Validations, sign in user

--- src/Pages/Private/Programming/Validations/Validations.jsx
+++ src/Pages/Private/Programming/Validations/Validations.jsx
@@ -18,6 +18,10 @@ const Validations = () => {
           <span>Send email to recover password</span>
           <Link target="_blank" to="https://docs.google.com/document/d/1zLgLU7if9P0cMxyPYqDI5mKu9hPIx_5mzx8PyCDsmIM/edit?usp=drive_link"><span>Link</span></Link>
         </div>
+        <div className='Card'>
+          <span>Send email to recover password</span>
+          <Link target="_blank" to="https://docs.google.com/document/d/1l_rEO56soLYtemNOBGPt005rMih5l3-uRHFkloNZqpo/edit?usp=drive_link"><span>Link</span></Link>
+        </div>
       </div>
     </div>
   )
