4cf289c180e089a5d36dd8804a81a8c839caea6b
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-24 18:39:07 +0100

    Validations page, translations

--- src/Pages/Private/Programming/Validations/Validations.jsx
+++ src/Pages/Private/Programming/Validations/Validations.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import './Validations.scss';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const Validations = () => {
+
+  const { t } = useTranslation()
+
   return (
     <div className='Validations'>
       <div className='Validations_Functions'>
         <div className='Card'>
-          <span>Sign in employee</span>
+          <span>{t('Sign in employee')}</span>
           <Link target="_blank" to="https://docs.google.com/document/d/1qUhXftXPdI-7o6BzpHeVF3K3EQ7PsTZF_Ex5j6aWUrw/edit?usp=drive_link"><span>Link</span></Link>
         </div>
         <div className='Card'>
-          <span>Sign up user</span>
+          <span>{t('Sign up user')}</span>
           <Link target="_blank" to="https://docs.google.com/document/d/1l_rEO56soLYtemNOBGPt005rMih5l3-uRHFkloNZqpo/edit?usp=drive_link"><span>Link</span></Link>
         </div>
         <div className='Card'>
-          <span>Send email to recover password</span>
+          <span>{t('Send email to recover password')}</span>
           <Link target="_blank" to="https://docs.google.com/document/d/1zLgLU7if9P0cMxyPYqDI5mKu9hPIx_5mzx8PyCDsmIM/edit?usp=drive_link"><span>Link</span></Link>
         </div>
         <div className='Card'>
-          <span>Sign in user</span>
+          <span>{t('Sign up user')}</span>
           <Link target="_blank" to="https://docs.google.com/document/d/1l_rEO56soLYtemNOBGPt005rMih5l3-uRHFkloNZqpo/edit?usp=drive_link"><span>Link</span></Link>
         </div>
         <div className='Card'>
-          <span>Reset password user</span>
+          <span>{t('Reset password user')}</span>
           <Link target="_blank" to="https://docs.google.com/document/d/1y7UFNL3lQXREBF-cRa5wUH2HfOBqpdkyWmDXelUkMXI/edit?usp=drive_link"><span>Link</span></Link>
         </div>
       </div>
