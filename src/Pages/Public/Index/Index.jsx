70e950988919539a1d48c3b4979f5e817e6b8572
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-09-10 03:49:24 +0100

    Index page, translation

--- src/Pages/Public/Index/Index.jsx
+++ src/Pages/Public/Index/Index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Index.scss';
 import Global_Button from '../../../Components/Buttons/Global_Button/Global_Button';
+import { useTranslation } from 'react-i18next';
 
 const Index = () => {
+
+  const { t } = useTranslation();
+
   return (
     <div className='Index'>
 
       <div className="Button_Field">
         <Global_Button 
-          text='Login'
+          text={t('Sign in')}
+          to='Sign_In'
         />
       </div>
 
